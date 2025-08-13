3a7fdb53e441eb8fe2153ce0a3666192b613592d
Augusto <62222041+coffeasack@users.noreply.github.com>
2025-08-13 03:12:13 -0300

    comentários

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ function adicionarAmigo() {
         alert("Parece que faltou um nome.");
     }
 };
+//Nesta função, nós pegamos o valor do elemento "amigo" no HTML e o inserimos no array novoAmigoDoCoffeasack, mas só caso o nome não esteja vazio. Depois, limpamos a linha com a função abaixo.
 
 function limparLinha() {
     document.getElementById("amigo").value = "";
@@ -24,6 +25,8 @@ function listarAmigo() {
     document.getElementById("listaAmigos").innerHTML = textLista;
 };
 
+//Desta vez, a função limpa a barra de digitação de nomes para que haja uma nova entrada no futuro. Depois, chama a função listarAmigo, que adiciona um nome na lista logo depois que a barra de digitação é limpa.
+
 function sortearAmigo() {
     if (novoAmigoDoCoffeasack.length == 0) {
         alert("Não há amigos para sortear");
@@ -32,4 +35,6 @@ function sortearAmigo() {
         alert(`Seu amigo sorteado foi: ${amigoSorteado}`);
         document.getElementById("resultado").innerHTML = `<p>Seu amigo sorteado foi: ${amigoSorteado}</p>`
     }
-}
+};
+
+//Aqui nós usamos o Math.floor para arredondar o número de indices(index ou "slots" do array) que o Math.random define ao multiplicá-lo pelo número de slots do novoAmigoDoCoffeasack. Não adicionei o método para impedir que o último sorteado seja novamente sorteado.
